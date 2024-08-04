ab2ee9df2a49e2eb4bdc613a44e20aeb7ae455dd
testalagussa <120222054+Muhammed-Dayeh@users.noreply.github.com>
2024-08-04 15:38:33 +0300

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,27 @@
-import React from "react"
+import React from "react";
 
-
-
-const Modal = ({isVisible, errorMessage = null}: {isVisible: boolean, errorMessage?: string | null}) => {
+const Modal = ({
+  isVisible,
+  errorMessage = null,
+}: {
+  isVisible: boolean;
+  errorMessage?: string | null;
+}) => {
   if (isVisible) {
     return (
       <div className="absolute bg-[#0007] w-full h-full flex justify-center items-center">
         <div className="bg-white text-green-600 w-11/12 text-center font-bold text-3xl p-8">
-          {/* <h1>The Form Has Been  Submitted Successfully </h1> */}
-          <h1 style={{color: errorMessage ? "red" : "green"}}>{errorMessage != null ? errorMessage : "The Form Has Been  Submitted Successfully"}</h1>
+          <h1 style={{ color: errorMessage ? "red" : "green" }}>
+            {errorMessage != null
+              ? errorMessage
+              : "The Form Has Been  Submitted Successfully"}
+          </h1>
         </div>
       </div>
     );
   } else {
-    return(<></>)
+    return <></>;
   }
-  
 };
 
-
-
-
-export default Modal
+export default Modal;
